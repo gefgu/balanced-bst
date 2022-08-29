238b5608347eb63b9754049bfff40275337e7b2a
gefgu <gefgu@hotmail.com>
2022-08-29 14:40:56 -0300

    Add splitting of initial array in left and right

--- bst.js
+++ bst.js
@@ -10,7 +10,17 @@ function Tree(array) {
   function buildTree(arr) {
     arr = [...new Set(arr)].sort((a, b) => a - b); // Sort and remove duplicates
 
+    const midIndex = Math.floor(arr.length / 2);
+    const root = Node(
+      arr[midIndex],
+      arr.slice(0, midIndex),
+      arr.slice(midIndex + 1)
+    );
+
+    console.log(root);
+
     console.log(arr);
+    console.log(midIndex);
   }
 
   const root = buildTree(array);
