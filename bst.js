d0b2d53ac073cdc530c21a278436a530a2cf9162
gefgu <gefgu@hotmail.com>
2022-08-29 14:57:19 -0300

    Add creation of balanced tree

--- bst.js
+++ bst.js
@@ -1,6 +1,6 @@
-function Node(value, left, right) {
+function Node(data, left, right) {
   return {
-    value,
+    data,
     left,
     right,
   };
@@ -9,22 +9,16 @@ function Node(value, left, right) {
 function Tree(array) {
   function buildTree(arr) {
     arr = [...new Set(arr)].sort((a, b) => a - b); // Sort and remove duplicates
-
+    if (arr.length === 0) return null;
     const midIndex = Math.floor(arr.length / 2);
-    const root = Node(
-      arr[midIndex],
-      arr.slice(0, midIndex),
-      arr.slice(midIndex + 1)
-    );
-
-    console.log(root);
-
-    console.log(arr);
-    console.log(midIndex);
+    const left = arr.slice(0, midIndex);
+    const rigth = arr.slice(midIndex + 1);
+    const root = Node(arr[midIndex], buildTree(left), buildTree(rigth));
+    return root;
   }
 
   const root = buildTree(array);
-  return { root };
+  return root;
 }
 
 export default Tree;
